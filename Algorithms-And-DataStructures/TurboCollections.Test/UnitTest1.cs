414fc0f680189d626630e70805296f01e23065c4
reynin <144011447+reynin@users.noreply.github.com>
2024-05-06 17:03:46 +0200

    Resetup

---
+++ Algorithms-And-DataStructures/TurboCollections.Test/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace TurboCollections.Test;
+
+public static class TurboMathsTests
+{
+    [Test]
+    public static void SayHelloExists()
+    {
+        TurboMaths.SayHello();
+        
+        Assert.Pass();
+    }
+}
---
+++ Algorithms-And-DataStructures/TurboCollections/TurboMaths.cs
@@ -0,0 +1,9 @@
+namespace TurboCollections;
+
+public static class TurboMaths
+{
+    public static void SayHello()
+    {
+        Console.WriteLine($"Hello, I'm {typeof(TurboMaths).FullName}");
+    }
+}
