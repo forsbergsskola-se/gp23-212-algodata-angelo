9a662c7e6703c02259955b7e0d384d7630d023a5
reynin <144011447+reynin@users.noreply.github.com>
2024-05-06 23:10:50 +0200

    Start GameStateHistory exercise

---
+++ Algorithms-And-DataStructures/GameStateHistory/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+
+
+var currentGameState = new TurboLinkedStack<GameState>();
+
+while (true)
+{
+    Console.WriteLine($"You are here: {currentGameState}\n" +
+                      $"What do you want to do?\n" +
+                      $"(0): Go to Level 1\n" +
+                      $"(1): Go to Settings\n" +
+                      $"(2): Go Back" +
+                      $"(Q): Quit");
+}
+
+
+enum GameState
+{
+    MainMenu,
+    Setting,
+    Quit,
+    Level1,
+    Level2,
+    Level3
+}
