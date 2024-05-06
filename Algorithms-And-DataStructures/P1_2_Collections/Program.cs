0e95197861469032cb356ff7c39d86ff0b03468d
reynin <144011447+reynin@users.noreply.github.com>
2024-05-06 17:13:48 +0200

    P1_2

---
+++ Algorithms-And-DataStructures/P1_2_Collections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+List <int> list = new List<int>() {137, 1000, -200 };
+
+for (int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i]);
+}
+
+ArrayList multiType = new ArrayList() {true, "Forsbergs", 'a', 1000, .12f};
+
+for (int i = 0; i < multiType.Count; i++)
+{
+    Console.Write(multiType[i] + " ");
+}
