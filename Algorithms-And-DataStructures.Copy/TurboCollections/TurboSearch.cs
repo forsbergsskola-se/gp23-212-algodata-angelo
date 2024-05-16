4a724f2ff6b0db2141ae1ebce9a7470f338ff0ba
reynin <144011447+reynin@users.noreply.github.com>
2024-05-16 13:20:17 +0200

    BinarySearch Method

--- Algorithms-And-DataStructures.Copy/TurboCollections/TurboSearch.cs
+++ Algorithms-And-DataStructures.Copy/TurboCollections/TurboSearch.cs
@@ -15,4 +15,27 @@ public class TurboSearch
         }
         return -1; // =Search unsuccessful
     }
+
+    public static int BinarySearch<T>(IList<T> list, T targetValue) where T : IComparable<T>
+    {
+        int lowerBound = 0;
+        int upperBound = list.Count - 1;
+
+        while (upperBound >= lowerBound)
+        {
+            int midPoint = (lowerBound + upperBound) / 2;
+
+            int compTResult = targetValue.CompareTo(list[midPoint]); // Either returns 0, - or +
+            
+            if (compTResult == 0)
+            {
+                return midPoint;
+            } 
+            else if (compTResult < 0) { upperBound = midPoint--; //Set new upper limit
+            }
+            else { lowerBound = midPoint++; // Set new lower limit
+            }
+        }
+        return -1; //If search is unsuccessful
+    }
 }
